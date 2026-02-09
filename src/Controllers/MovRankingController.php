054ab543ec416fdb136ed7b109abf94d1bb8b6c3
Ricardo Farias <ricardo.farias@rentcars.com>
2026-02-08 21:41:21 -0300

    feat: fluxo primitivo da controller desenvolvido

--- src/Controllers/MovRankingController.php
+++ src/Controllers/MovRankingController.php
@@ -2,10 +2,34 @@
 
 namespace App\Controllers;
 
+use App\Database\Connection;
+use App\Repository\RankingRepository;
+use App\Services\MovRankingService;
+
 class MovRankingController
 {
-    public function index()
+    public function show()
     {
-        echo json_encode(['message' => 'ola mundo da controller de ranking']);
+        $movementParam = $_GET['movement'];
+
+        if (empty($movementParam)) {
+            http_response_code(400);
+            echo json_encode(['error' => 'Movement parameter is required']);
+            return;
+        }
+
+        $dbConnection = Connection::getInstance();
+        $rankingRepository = new RankingRepository($dbConnection);
+        $service = new MovRankingService($rankingRepository);
+        $ranking = $service->getRankingByMovement($movementParam, is_numeric($movementParam));
+
+        header('Content-Type: application/json');
+
+        if (isset($ranking)) {
+            echo json_encode($ranking);
+        } else {
+            http_response_code(400);
+            echo json_encode(['error' => 'Invalid movement parameter']);
+        }
     }
 }
